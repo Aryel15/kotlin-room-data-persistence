c261c8890a90af4ed237a9401d2d7ccc3be46c1d
Aryel Anne Louise de Souza Gabriel <aannelouise@outlook.com>
2024-11-09 22:47:04 -0300

    Inventory

--- app/src/main/java/com/example/inventory/data/AppContainer.kt
+++ app/src/main/java/com/example/inventory/data/AppContainer.kt
@@ -25,14 +25,8 @@ interface AppContainer {
     val itemsRepository: ItemsRepository
 }
 
-/**
- * [AppContainer] implementation that provides instance of [OfflineItemsRepository]
- */
 class AppDataContainer(private val context: Context) : AppContainer {
-    /**
-     * Implementation for [ItemsRepository]
-     */
     override val itemsRepository: ItemsRepository by lazy {
-        OfflineItemsRepository()
+        OfflineItemsRepository(InventoryDatabase.getDatabase(context).itemDao())
     }
 }
